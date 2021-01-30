cf22985d0f6e002e626621da042a8276f31baa8b
unknown <jonesrtremblay@gmail.com>
2021-01-29 20:32:49 -0500

    Ajouté le EyePassword. Doit régler la navbar      qui n'affiche pas bouton login selon si le user est connecté ou non.      A revoir.

--- PageLOGIN/myScript.js
+++ PageLOGIN/myScript.js
@@ -1,95 +1,33 @@
-var jonathan = {
-    "firstName" : "Jonathan",
-    "lastName" : "Tremblay",
-    "age" : 22,
-    "gender" : "Male",
-    "country" : "Canada",
-    "region" : "Montreal",
-    "username" : "jones",
-    "password" : "blackPink",
-    "tellUsMore": "Nothing to say.",
-    "photo" : "../media/avatar/exAvatar.jpg"
-}
-
-var fatima = {
-    "firstName" : "Fatima",
-    "lastName" : "Ghaddar",
-    "age" : 25,
-    "gender" : "Female",
-    "country" : "Canada",
-    "region" : "Montreal",
-    "username" : "fatima",
-    "password" : "regasu",
-    "tellUsMore": "Vive Haïti.", 
-    "photo" : "../media/avatar/dicaprio.jpg"
-
-}
-var dicaprio = {
-    "firstName" : "Leonardo",
-    "lastName" : "DiCaprio",
-    "age" : 46,
-    "gender" : "Male",
-    "country" : "USA",
-    "region" : "Los Angeles",
-    "username" : "dicaprio",
-    "password" : "nature",
-    "tellUsMore": "I'm an actor.",
-    "photo" : "../media/avatar/fatima.jpg"
-
-}
-var freeman = {
-    "firstName" : "Morgan",
-    "lastName" : "Freeman",
-    "age" : 83,
-    "gender" : "Male",
-    "country" : "USA",
-    "region" : "Memphis",
-    "username" : "freeman",
-    "password" : "morgan",
-    "tellUsMore": "Gravitas.",
-    "photo" : "../media/avatar/freeman.jpg"
-
-}
-
-var users = [jonathan, fatima, dicaprio, freeman];
-
-
-
-
-function getUserByUsername(username){
-    for (i = 0; i < users.length; i++){
-        if (users[i].username == username){
-            return users[i];
-        }
-    }
-}
-
-
 $(function(){
     $("#btnLogin").on("click", function(){
-        let user = $("#inputUser").val();
-        let pass = $("#inputPass").val();
-        if (user == getUserByUsername(user).username){
-            if (pass == getUserByUsername(user).password){
-                window.location.replace("../PageAcceuil/index.html");
-            }
-        }
+        
+        seConnecter();
     });
 });
 
-function settingsWithoutLogin(showHide){
-    if (showHide == "show"){
-        $("#myModal").modal("toggle");
+
+function seConnecter(){
+    let user = $("#inputUser").val();
+    let pass = $("#inputPass").val();
+    if (user == "" || pass == ""){
+        alert("Please enter a username and a password.")
+    } else if (user == getUserByUsername(user).username){
+        if (pass == getUserByUsername(user).password){
+            connected = true;
+            window.location.replace("../PageAcceuil/index.html");
+
+        }
     }
-    if (showHide == "hide"){
-        $("#myModal").modal("hide");
-    }
+    
 }
 
-function showHidePasswordField(){
-    if ($("#inputPass").attr("type") == "password") {
-        $("#inputPass").attr("type", "text");
+function showPasswordField(id, el) {
+    let x = document.getElementById(id);
+    if (x.type === "password") {
+      x.type = "text";
+      el.className = 'fa fa-eye-slash fa-2x showpwd';
     } else {
-        $("#inputPass").attr("type", "password");
+      x.type = "password";
+      el.className = 'fa fa-eye fa-2x showpwd';
     }
-}
+  } 
---
+++ js/userInfo.js
@@ -0,0 +1,87 @@
+connected = false;
+
+// var connection = localStorage.setItem("connected", "false");
+
+var jonathan = {
+    "firstName" : "Jonathan",
+    "lastName" : "Tremblay",
+    "age" : 22,
+    "gender" : "Male",
+    "country" : "Canada",
+    "region" : "Montreal",
+    "username" : "jones",
+    "password" : "blackPink",
+    "tellUsMore": "Nothing to say.",
+    "photo" : "../media/avatar/exAvatar.jpg"
+}
+
+var fatima = {
+    "firstName" : "Fatima",
+    "lastName" : "Ghaddar",
+    "age" : 25,
+    "gender" : "Female",
+    "country" : "Canada",
+    "region" : "Montreal",
+    "username" : "fatima",
+    "password" : "regasu",
+    "tellUsMore": "Vive Haïti.", 
+    "photo" : "../media/avatar/dicaprio.jpg"
+
+}
+var dicaprio = {
+    "firstName" : "Leonardo",
+    "lastName" : "DiCaprio",
+    "age" : 46,
+    "gender" : "Male",
+    "country" : "USA",
+    "region" : "Los Angeles",
+    "username" : "dicaprio",
+    "password" : "nature",
+    "tellUsMore": "I'm an actor.",
+    "photo" : "../media/avatar/fatima.jpg"
+
+}
+var freeman = {
+    "firstName" : "Morgan",
+    "lastName" : "Freeman",
+    "age" : 83,
+    "gender" : "Male",
+    "country" : "USA",
+    "region" : "Memphis",
+    "username" : "freeman",
+    "password" : "morgan",
+    "tellUsMore": "Gravitas.",
+    "photo" : "../media/avatar/freeman.jpg"
+
+}
+
+var users = [jonathan, fatima, dicaprio, freeman];
+
+$(function(){
+    if(connected == true){
+        $("#navConnection").html($("#navConnection").html().replace('( Login )','( Logout )'));
+
+    } else {
+        $("#navConnection").html($("#navConnection").html().replace('( Logout )', '( Login )'));
+
+    }
+    $("#navConnection").attr("href", "../PageLogin/login.html");
+})
+
+function settingsWithoutLogin(showHide){
+    if (showHide == "show"){
+        $("#myModal").modal("toggle");
+    }
+    if (showHide == "hide"){
+        $("#myModal").modal("hide");
+    }
+}
+
+function getUserByUsername(username){
+    for (i = 0; i < users.length; i++){
+        if (users[i].username == username){
+            return users[i];
+        }
+    }
+}
+
